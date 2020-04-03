d0525f9b4ef04dfd70e4598e81df33f16d67e639
Benjamin Thom <jaminthom@gmail.com>
2020-04-03 10:44:12 -0700

    update ParksLookup/Models/ParksLookupContext.cs

---
+++ ParksLookup/Models/ParksLookupContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksLookup.Models
+{
+  public class ParksLookupContext : DbContext
+  {
+    public ParksLookupContext(DbContextOptions<ParksLookupContext> options)
+      :base(options)
+      {
+
+      }
+
+    public DbSet<Park> Parks { get; set; }
+    public DbSet<Detail> Details { get; set; }
+
+  }
+}
